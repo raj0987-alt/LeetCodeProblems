b3cf3563cee913b485eacd3f27b33f58825477f0
raj0987-alt <raj.tazimul2011@gmail.com>
2024-11-22 21:44:38 +0600

    Valid Palindrome

---
+++ 125. Valid Palindrome/125. Valid Palindrome/Program.cs
@@ -0,0 +1,15 @@
+﻿using _125._Valid_Palindrome;
+
+Solution solution = new Solution();
+string s;
+Console.WriteLine("Enter The String: ");
+s = Console.ReadLine();
+bool IsPalindrome = solution.IsPalindrome(s);
+if (IsPalindrome)
+{
+    Console.WriteLine($"{s} Is a Valid Palindrome. The Case Is : {IsPalindrome}");
+}
+else
+{
+    Console.WriteLine($"{s} Is Not a Valid Palindrome. The Case Is : {IsPalindrome}");
+}
---
+++ 125. Valid Palindrome/125. Valid Palindrome/Solution.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _125._Valid_Palindrome
+{
+    public class Solution
+    {
+
+        public bool IsPalindrome(string s)
+        {
+            int x = 0, y = s.Length - 1;
+
+            while (x <= y)
+            {
+                char a = s[x]; char b = s[y];
+                if (!char.IsLetterOrDigit(a))
+                {
+                    x++;
+                }
+                else if (!char.IsLetterOrDigit(b))
+                {
+                    y--;
+                }
+                else
+                {
+                    if (char.ToLower(a) != char.ToLower(b))
+                        return false;
+                    x++; y--;
+                }
+
+
+            }
+            return true;
+
+        }
+
+
+        // With Regex
+
+        public bool PainDromeCheck(string s, int i, int j)
+        {
+            if (i >= j) return true;
+            if(s[i] != s[j]) return false;
+            i++; j--;
+            return PainDromeCheck(s, i, j);
+            
+
+        }
+
+        public bool IsPalindromeWithRegex(string s)
+        {
+            s = Regex.Replace(s, "[^A-Za-z0-9]", "");
+
+            s = s.ToLower();
+
+            int x = 0, y = s.Length - 1;
+
+            return PainDromeCheck(s, x, y);
+
+        }
+        //public bool IsPalindrome(string s)
+        //{
+        //    s = Regex.Replace(s, "[^A-Za-z0-9]", "");
+
+        //    s = s.ToLower();
+
+        //    int x = 0, y = s.Length - 1;
+
+        //    while (x <= y)
+        //    {
+        //        char a = s[x], b = s[y];
+        //        if (a !=b)
+        //        {
+        //            return false;
+        //        }
+        //        x++; y--;
+        //    }
+        //    return true;
+
+        //}
+
+
+
+
+
+    }
+}
