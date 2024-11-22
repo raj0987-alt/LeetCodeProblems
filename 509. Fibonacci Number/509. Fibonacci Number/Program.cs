c8e678f2c98f5caf4d3aeab69d01ceeb06c84e52
raj0987-alt <raj.tazimul2011@gmail.com>
2024-11-22 18:10:20 +0600

    Initial Commit

---
+++ 509. Fibonacci Number/509. Fibonacci Number/Program.cs
@@ -0,0 +1,10 @@
+﻿using _509._Fibonacci_Number;
+
+Solution solution = new Solution();
+
+int n;
+Console.WriteLine($"Enter The Desire Number: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+
+//Console.WriteLine($"The Fibonacci Number of {n} Is: {solution.Fib(n)}");
---
+++ 509. Fibonacci Number/509. Fibonacci Number/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _509._Fibonacci_Number
+{
+    public class Solution
+    {
+        // Implementing By Recursion
+        public int Fib(int n)
+        {
+            int f = 0;
+            
+            return f;
+        }
+
+        // Implementing By Iterating
+        public int ItrFib(int n)
+        {
+            if (n == 1 || n == 2)
+                return 1;
+
+            int n1 = 1, n2 = 1, f = 0;
+
+            for (int i = 3; i <= n; i++)
+            {
+                f = n1 + n2;
+                n1 = n2;
+                n2 = f;
+            }
+
+            return f;
+        }
+
+        
+        
+    }
+}
