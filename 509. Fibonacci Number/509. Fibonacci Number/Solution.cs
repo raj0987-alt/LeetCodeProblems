2e9fc1606872b1ecacf841276e48a06323bc05d7
raj0987-alt <raj.tazimul2011@gmail.com>
2024-11-22 19:14:08 +0600

    Recursion Apply

--- 509. Fibonacci Number/509. Fibonacci Number/Solution.cs
+++ 509. Fibonacci Number/509. Fibonacci Number/Solution.cs
@@ -9,31 +9,72 @@ namespace _509._Fibonacci_Number
     public class Solution
     {
         // Implementing By Recursion
-        public int Fib(int n)
+
+        public int Fib(int n, Dictionary<int, int> memo = null)
         {
+            if (memo == null)
+                memo = new Dictionary<int, int>();
+
+            if (n == 0)
+                return 0;
+            if (n == 1 || n == 2)
+                return 1;
+
+            if (memo.ContainsKey(n))
+                return memo[n];
+
+            memo[n] = Fib(n - 1, memo) + Fib(n - 2, memo);
+            return memo[n];
+
+        }
+
+        public int FibUn(int n)
+        {
+            Dictionary<int, int> memo = new Dictionary<int, int>();
             if (n == 1 || n == 2)
                 return 1;
             if (n == 0)
                 return 0;
-            return Fib(n - 1) + Fib(n - 2);
+            if(memo.ContainsKey(n))
+            {
+                return memo[n];
+            }
+            if (!memo.ContainsKey(n))
+                memo.Add(n, Fib(n - 1, memo) + Fib(n - 2, memo));
+
+            return memo[n];
         }
+
+
+
+
 
         // Implementing By Iterating
         public int ItrFib(int n)
         {
-            if (n == 1 || n == 2)
-                return 1;
+            //if (n == 1 || n == 2)
+            //    return 1;
 
-            int n1 = 1, n2 = 1, f = 0;
+            //int n1 = 1, n2 = 1;
 
-            for (int i = 3; i <= n; i++)
+            //for (int i = 3; i <= n; i++)
+            //{
+            //    int temp = n2;
+            //    n2 = n1 + n2;
+            //    n1 = temp;
+            //}
+
+            //return n2;
+
+            if (n <= 1) return n;
+            int a = 0, b = 1;
+            for (int i = 2; i <= n; i++)
             {
-                f = n1 + n2;
-                n1 = n2;
-                n2 = f;
+                int temp = b;
+                b = a + b;
+                a = temp;
             }
-
-            return f;
+            return b;
         }
 
         
